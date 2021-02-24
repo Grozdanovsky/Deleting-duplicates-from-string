39f5f4911d7adae292b86a414c750666f8edc5f9
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-24 20:01:37 +0100

    Cutting duplicate strings

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def cutting_string1(w1,w2):
+    return word1.replace(word2 , "").strip()
+
+# MAIN
+
+word1 = "first example"
+word2 = "first"
+
+final1 = cutting_string1(word1,word2)
+print(final1)
