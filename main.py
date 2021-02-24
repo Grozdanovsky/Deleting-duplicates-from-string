bfea7baed818504ad1ad7b2109009ec304aa6328
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-24 20:20:09 +0100

    list to string

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def cutting_string2(w1,w2):
 
     return list3
 
+def list_to_string(list1):
+
+    return " ".join(list1)
 
 
 # MAIN
@@ -22,6 +25,7 @@ word2 = "first cefi"
 
 final1 = cutting_string1(word1,word2)
 final2 = cutting_string2(word1,word2)
+final3 = list_to_string(final2)
 print(final1)
 print(final2)
-
+print(final3)
